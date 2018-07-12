c655c186cff7178e2b13717cdfb2b922266a69ec
r <787884981@qq.com>
2018-07-13 00:02:28 +0800

    header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>Header</div>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +20,33 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+  @import "../../../assets/styles/varibles.styl"
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align:center
+        font-size:.4rem
+    .header-input
+      flex: 1
+      height:.64rem
+      line-height:.64rem
+      margin-top:.12rem
+      margin-left:.2rem
+      padding-left:.2rem
+      background :#fff
+      border-radius :.1rem
+      color:#ccc
+    .header-right
+      width: 1.24rem
+      text-align: center
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
 </style>
